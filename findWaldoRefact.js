434cad065af910ab6c670ee1a60e50829379d63f
Jorgelduarte <duarte.junior@gmail.com>
2018-02-15 19:38:09 +0000

    findWaldo now is using forEach

---
+++ findWaldoRefact.js
@@ -0,0 +1,13 @@
+function findWaldo(arr, found) {
+  arr.forEach(function (element, i){
+    if (element === "Waldo"){
+      found(i);
+    } 
+  });
+}
+
+function actionWhenFound(index) {
+  console.log("Found him at index " + index + " !");
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
